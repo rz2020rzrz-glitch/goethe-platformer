57543120fa79608f821353448de1176b83fdd613
rz2020rzrz-glitch <rz2020rzrz@gmail.com>
2026-03-24 19:19:44 +0100

    Aktualisiere tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -10,6 +10,10 @@ namespace myTiles {
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -48,6 +52,10 @@ namespace myTiles {
             case "tile3":return tile3;
             case "myTile":
             case "tile4":return tile4;
+            case "Player":
+            case "tile5":return tile5;
+            case "Ende":
+            case "tile6":return tile6;
         }
         return null;
     })
